de64e4b46ff2be119e8e320b53125cce77dcb88e
capital terefe <capital_terefe@ao.uscourts.gov>
2018-08-30 15:24:05 -0400

    added jenkins fetch request

--- src/App.js
+++ src/App.js
@@ -3,6 +3,43 @@ import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
+
+   buildUrl(url, parameters) {
+    let qs = "";
+    for (const key in parameters) {
+        if (parameters.hasOwnProperty(key)) {
+            const value = parameters[key];
+            qs +=
+                encodeURIComponent(key) + "=" + encodeURIComponent(value) + "&";
+        }
+    }
+    if (qs.length > 0) {
+        qs = qs.substring(0, qs.length - 1); //chop off last "&"
+        url = url + "?" + qs;
+    }
+
+    return url;
+}
+    callJenkins=()=>{
+      console.log('fired Jenkins');
+      return fetch(
+        buildUrl("http://automationap.aocms.gtwy.dcn:9090/job/AP_1.4/buildWithParameters?", {
+          tag:"@demo",
+          courtId="cm3a",
+          PlanId="TESTAOM",
+          numberOfRunners=3,
+          auth: {
+            'user': 'admin',
+            'pass': 'admin'
+          }
+        }),
+        {
+            method: "POST",
+            
+        }
+    )
+    }
+
   render() {
     return (
       <div className="App">
@@ -13,6 +50,9 @@ class App extends Component {
         <p className="App-intro">
           Welcome...
         </p>
+        <button onClick={this.callJenkins}>
+        Fire Jenkins
+       </button>
       </div>
     );
   }
